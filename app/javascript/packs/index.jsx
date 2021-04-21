4681f7fcb44d95c3dcf29abf3de514e5894b0966
akash chhetri <akash.chhetri1@gmail.com>
2021-04-21 13:46:55 +1000

    conflict resolved and documents updated with correct linting

--- app/javascript/packs/index.jsx
+++ app/javascript/packs/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
+import App from '../react/components/App';
 
 document.addEventListener('DOMContentLoaded', () => {
 	ReactDOM.render(<App />, document.body.appendChild(document.createElement('div')));
