9c45a46ffb945904e5a437f15047cd518647c0ac
Michael Milewski <saramic@gmail.com>
2021-04-25 12:12:37 +1000

    date cannot be nil and enforce unique key

--- db/migrate/20210425014657_create_daily_stats.rb
+++ db/migrate/20210425014657_create_daily_stats.rb
@@ -1,11 +1,13 @@
 class CreateDailyStats < ActiveRecord::Migration[6.1]
   def change
     create_table :daily_stats, id: :uuid do |t|
-      t.date :date
+      t.date :date, null: false
       t.jsonb :data
       t.references :user, null: false, foreign_key: true, type: :uuid
 
       t.timestamps
     end
+
+    add_index(:daily_stats, [:user_id, :date], unique: true)
   end
 end
