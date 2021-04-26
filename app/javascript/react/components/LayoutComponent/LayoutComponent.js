5daeae80eb998a1e03fdebef1a38f9b2cadd9478
akash chhetri <akash.chhetri1@gmail.com>
2021-04-26 10:42:02 +1000

    React router added, Layout Container styled, Navbar and Footer component added
    layout container is styled with fixed footer in the bottom using grid.

--- app/javascript/react/components/LayoutComponent/LayoutComponent.js
+++ app/javascript/react/components/LayoutComponent/LayoutComponent.js
@@ -2,14 +2,19 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Header from "../Header/Header";
 import Home from "../../pages/Home/Home";
+import Footer from "../Footer/Footer";
+import style from "./layout.module.scss";
 
 const LayoutComponent = () => {
   return (
-    <BrowserRouter>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={Home} />
-      </Switch>
+    <BrowserRouter className="akash">
+      <div className={style.container}>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Home} />
+        </Switch>
+        <Footer />
+      </div>
     </BrowserRouter>
   );
 };
