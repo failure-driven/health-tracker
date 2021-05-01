fbe00857a0598d4c7f4443b83ade48439af1b0ec
Michael Milewski <saramic@gmail.com>
2021-05-01 11:13:56 +1000

    date and formatting tweak

--- app/javascript/react/pages/Stats/Stats.js
+++ app/javascript/react/pages/Stats/Stats.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import gql from "graphql-tag";
+import moment from 'moment';
 import { useQuery } from "@apollo/client";
 
 const TEST_QUERY = gql`
@@ -21,18 +22,18 @@ const Stats = () => {
     <ul className="list-group">
       <li className="list-group-item">
         <div className="row">
-          <div className="col-sm-3"><strong>Date</strong></div>
-          <div className="col-sm-9"><strong>Data</strong></div>
+          <div className="col-md-2"><strong>Date</strong></div>
+          <div className="col-md-10"><strong>Data</strong></div>
         </div>
       </li>
       {data.dailyStats.map((stat) => (
         <li key="{stat.id}" className="list-group-item">
           <div className="row">
-            <div className="col-sm-3">{stat.date}</div>
-            <div className="col-sm-9">{Object.keys(stat.data).map((key) => (
+            <div className="col-md-2">{moment(stat.date).format("L")}</div>
+            <div className="col-md-10">{Object.keys(stat.data).map((key) => (
               <dl className="row">
-                <dt className="col-2">{key}</dt>
-                <dd>{stat.data[key]}</dd>
+                <dt className="col-4 text-right">{key}</dt>
+                <dd className="col-8">{stat.data[key]}</dd>
               </dl>
             ))}</div>
           </div>
