1d7860aa42f29168f15ccea3c1823da0cdd0c4d6
Michael Milewski <saramic@gmail.com>
2021-08-21 20:05:15 +1000

    [📟¼] simplify page fragments to use page

--- spec/support/page_fragments.rb
+++ spec/support/page_fragments.rb
@@ -1,26 +1,18 @@
 module PageFragments
-  Page = Struct.new :rspec_example do
-    # Capybara DSL + rspec example context
-    alias_method :browser, :rspec_example
-  end
-
-  module DelegateSynchronize
-    def synchronize(...)
-      page.document.synchronize(...)
-    end
-  end
-
-  def classify(string)
-    string.to_s.split("_").map(&:capitalize).join
+  Page = Struct.new(:rspec_example) do
+    alias_method :page, :rspec_example
   end
 
   def focus_on(*args)
-    require File.join(__dir__, "page_fragments", args.map(&:to_s))
-    mod = args.inject(PageFragments) do |klass, sub_klass|
-      klass.const_get(classify(sub_klass))
+    require File.join(args.map(&:to_s))
+    page = Page.new self
+    page_fragment = args
+      .map(&:to_s)
+      .map(&:camelize)
+      .reduce(PageFragments) do |module_name, part|
+      module_name.const_get(part, false)
     end
-    page = Page.new(self).extend(mod)
-    page.browser.extend(DelegateSynchronize)
+    page = page.extend(page_fragment)
     yield page if block_given?
     page
   end
