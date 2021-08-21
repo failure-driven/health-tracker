89051f5ce39360734652ecfb3a0816dd314d7855
Michael Milewski <saramic@gmail.com>
2021-08-21 16:48:00 +1000

    Pending spec around saving numeric values
    Hey Ben, we need your awesome code to change string input in form to be
    numeric prior to submitting back to server. Also note once done, you
    will need to remove the "pending" message - and there seem to be some
    saved blank values? not sure if you need to remove them.
    
    Co-authored-by: Ben Ratelade <ratelade.benjamin@gmail.com>

---
+++ spec/features/user_adds_stats_using_nice_ui_spec.rb
@@ -0,0 +1,79 @@
+require "rails_helper"
+
+feature "User adds stats using nice UI", js: true do
+  context "A user existst with stats" do
+    before do
+      @user_claudia = User.create!(
+        email: "claudia.king@automio.com",
+        password: "1password",
+        confirmed_at: DateTime.now,
+      )
+      @user_claudia.daily_stats.create!(
+        date: Date.parse("2015-04-01"),
+        data: {
+          weight: 66.6,
+          situps: 100,
+        },
+      )
+      @user_claudia.daily_stats.create!(
+        date: Date.parse("2015-04-02"),
+        data: {
+          weight: 66.6,
+          situps: 80,
+        },
+      )
+    end
+
+    scenario "Claudia signs in, sees her stats and updates them" do
+      When "Claudia signs in and views her stats in admin" do
+        visit root_path
+        page.find("nav a", text: "Log in").click
+        page.find("form.new_user").fill_in("Email", with: "claudia.king@automio.com")
+        page.find("form.new_user").fill_in("Password", with: "1password")
+        page.find("form.new_user").find('input[type="submit"]').click
+        # expect(
+        #   page.find("p.alert [data-testid=\"message\"]").text
+        # ).to eq "Signed in successfully."
+        page.find("nav a", text: "admin").click
+        page.find("nav.navigation a", text: "Daily Stats").click
+      end
+
+      Then "she sees her stats for April 1 and 2" do
+        expect(
+          page
+            .find_all("table tr td")
+            .filter { |td| td["class"] =~ /(--date|--jsonb)/ }
+            .map(&:text),
+        ).to eq([
+                  "2015-04-01", "{ \"situps\": 100, \"weight\": 66.6 }",
+                  "2015-04-02", "{ \"situps\": 80, \"weight\": 66.6 }",
+                ])
+      end
+
+      When "she uses the slick UI to add another stat" do
+        page.find("nav a", text: "Back to app").click
+        page.find("button", text: "add stats for today").click
+        page.find("input[name=activity]").send_keys("chinups")
+        page.find("input[name=answer]").send_keys("20")
+        page.all("button", text: "Add")[0].click
+        page.find("button", text: "Add this to my list").click
+      end
+
+      Then "she sees the new stat visible as well" do
+        page.find("nav a", text: "admin").click
+        page.find("nav.navigation a", text: "Daily Stats").click
+        pending "the React UI saving numeric data not string"
+        expect(
+          page
+            .find_all("table tr td")
+            .filter { |td| td["class"] =~ /(--date|--jsonb)/ }
+            .map(&:text),
+        ).to match([
+                     "2015-04-01", "{ \"situps\": 100, \"weight\": 66.6 }",
+                     "2015-04-02", "{ \"situps\": 80, \"weight\": 66.6 }",
+                     match(/.*/), "{ \"chinups\": 20 }",
+                   ])
+      end
+    end
+  end
+end
