abfb9539707568c093c7e297e3b57a930f202c58
Benjamin ratelade <ratelade.benjamin@gmail.com>
2021-08-29 10:32:27 +1000

    [Rubocop👮‍♂️] Fixing errors raised by make build

--- spec/support/factories/daily_stat.rb
+++ spec/support/factories/daily_stat.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :daily_stat do
     id { |index| index }
-    date { DateTime.now - rand(0...1460)}
-    data { { weight: 66.0 + rand(0.0...3.0), situps: 80 + rand(0...10) } }
+    date { DateTime.now - rand(0...1460) }
+    data { { weight: 66.0 + rand(0.0...3.0), situps: rand(70...100) } }
   end
 end
--- spec/support/factories/users.rb
+++ spec/support/factories/users.rb
@@ -2,11 +2,11 @@ FactoryBot.define do
   factory :user do
     id { |index| index }
     factory :user_claudia do
-      email {|index| "claudia.king@automio.com"}
+      email { "claudia.king@automio.com" }
       password { "1password" }
       confirmed_at { DateTime.now }
     end
-    
+
     factory :user_with_daily_stats do
       transient do
         posts_count { 5 }
